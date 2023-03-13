d2fd85bd38a77b7eb4ec1d5094f8585a2779007b
Haocheng Xi <xihc20@ent.cs.washington.edu>
2023-03-13 15:23:31 -0700

    partly successful to extract relations

--- Extract Relation/exact_match.py
+++ Extract Relation/exact_match.py
@@ -60,22 +60,25 @@ for file_path in Relation_path:
 
     print("The current time is:", time.strftime("%H:%M:%S", time.localtime()), "Finish Loading 3")
 
-    for triple in Triple_list:
+    for triple in tqdm(Triple_list):
         objs = triple["object"]
         obj_syns = triple["object_synonym"]
 
-        objs = flatten(objs)
-        obj_syns = flatten(obj_syns)
+        objs = flatten([objs])
+        obj_syns = flatten([obj_syns])
 
         objs.extend(obj_syns)
 
         subs = triple["subject"]
         sub_syns = triple["subject_synonym"]
-        subs = flatten(subs)
-        sub_syns = flatten(sub_syns)
+
+        subs = flatten([subs])
+        sub_syns = flatten([sub_syns])
+
         subs.extend(sub_syns)
 
-        union_objs_pmid = {}
+        # object
+        union_objs_pmid = set([])
         for obj in objs:
             words = tokenizer.tokenize(obj)
             for idx, word in enumerate(words):
@@ -89,24 +92,34 @@ for file_path in Relation_path:
                     common_obj_pmid = set(word_pmid)
                 else:
                     common_obj_pmid = common_obj_pmid.intersection(word_pmid)
-                # print(list(common_obj_pmid)[:5], list(common_obj_pmid)[-5:])
 
-            print("OBJ ", len(common_obj_pmid), obj)
+            actual_common_obj_pmid = set([])
             if len(common_obj_pmid):
                 for pmid in common_obj_pmid:
-                    article = Pubmed_data[pmid]
+                    if Pubmed_data[pmid][0][-1] in ['.', '?', '!']:
+                        article = Pubmed_data[pmid][0].replace('[', '').replace(']', '') + Pubmed_data[pmid][1]
+                    else:
+                        article = Pubmed_data[pmid][0].replace('[', '').replace(']', '') + '. ' + Pubmed_data[pmid][1]
+
                     if obj in article:
                         union_objs_pmid.add(pmid)
+                        actual_common_obj_pmid.add(pmid)
 
-        union_subs_pmid = {}
+            # if len(actual_common_obj_pmid):
+            #     print("OBJ ", obj)
+            #     print("OBJ ", len(common_obj_pmid), list(common_obj_pmid)[:5], list(common_obj_pmid)[-5:])
+            #     print("OBJ ", len(actual_common_obj_pmid), list(actual_common_obj_pmid)[:5], list(actual_common_obj_pmid)[-5:])
+
+        union_subs_pmid = set([])
         for sub in subs:
             words = tokenizer.tokenize(sub)
             for idx, word in enumerate(words):
+                print(word)
                 word = word.lower()
-                if word not in subect_dict.keys():
+                if word not in Subject_dict.keys():
                     print(f"CAN NOT FIND {word}")
                     continue
-                word_pmid = subect_dict[word]
+                word_pmid = Subject_dict[word]
                 # print(word, word_pmid[:5], word_pmid[-5:])
                 if idx == 0:
                     common_sub_pmid = set(word_pmid)
@@ -114,12 +127,26 @@ for file_path in Relation_path:
                     common_sub_pmid = common_sub_pmid.intersection(word_pmid)
                 # print(list(common_sub_pmid)[:5], list(common_suu_pmid)[-5:])
 
-            print("SUB ", len(common_sub_pmid), sub)
+            actual_common_sub_pmid = set([])
             if len(common_sub_pmid):
                 for pmid in common_sub_pmid:
-                    article = Pubmed_data[pmid]
+                    if Pubmed_data[pmid][0][-1] in ['.', '?', '!']:
+                        article = Pubmed_data[pmid][0].replace('[', '').replace(']', '') + Pubmed_data[pmid][1]
+                    else:
+                        article = Pubmed_data[pmid][0].replace('[', '').replace(']', '') + '. ' + Pubmed_data[pmid][1]
+
                     if sub in article:
                         union_subs_pmid.add(pmid)
+                        actual_common_sub_pmid.add(pmid)
 
-        union_relation_pmid = union_subs_pmid.intersection(union_objs_pmid)
-        print("RELATION ", len(union_relation_pmid), union_relation_pmid[:5], union_relation_pmid[-5:])
+            # if len(actual_common_sub_pmid):
+            #     print("SUB ", sub)
+            #     print("SUB ", len(common_sub_pmid), list(common_sub_pmid)[:5], list(common_sub_pmid)[-5:])
+            #     print("SUB ", len(actual_common_sub_pmid), list(actual_common_sub_pmid)[:5], list(actual_common_sub_pmid)[-5:])
+
+
+        union_relation_pmid = set(union_subs_pmid).intersection(set(union_objs_pmid))
+        print("RELATION ", len(union_relation_pmid), list(union_relation_pmid)[:5], list(union_relation_pmid)[-5:])
+        if len(union_relation_pmid):
+            result = pyfiglet.figlet_format(objs[0] + "  " + subs[0])
+            print(result)
